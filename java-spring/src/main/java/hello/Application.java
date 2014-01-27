3172fabf09d27a2869f66e7f78d8cb7ff3fa7c11
liunaijia <lnj@devt.me>
2014-01-27 17:25:29 +0800

    init

--- java-spring/src/main/java/hello/Application.java
+++ java-spring/src/main/java/hello/Application.java
@@ -3,9 +3,11 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
+@Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
