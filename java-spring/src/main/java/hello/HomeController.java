1fdfe34711624a960278385dae4ff61ac9264cdb
liunaijia <lnj@devt.me>
2014-01-28 17:28:34 +0800

    init

--- java-spring/src/main/java/hello/HomeController.java
+++ java-spring/src/main/java/hello/HomeController.java
@@ -1,6 +1,15 @@
 package hello;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +17,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
-    public String index() {
+    public String index(Model model) throws IOException {
+		Map<String, String> data = getData();
+		model.addAttribute(data);
+		
         return "index";
     }
 
+	private Map<String, String> getData() throws IOException {
+		Map<String, String> data = new HashMap<String, String>();
+		
+		Document doc = Jsoup.connect("http://www.infoq.com/cn/news/190").get();
+		for(Element e : doc.select("div.news_type_block")){
+			Element link = e.select("h2 a").first();
+			String url = link.attr("href");
+			String title = link.text();
+			data.put(url, title);
+		}
+		
+		return data;
+	}
 }
