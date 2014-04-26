5a06c116f5856db74470459997215e2fb8892b53
liunaijia <lnj@devt.me>
2014-04-26 22:47:21 +0800

    chapter 1

---
+++ ia/AlgorithmEfficiencyCompare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IA {
+    public class AlgorithmEfficiencyCompare {
+        private int GetMinNumberThatFunc1FasterThanFunc2(Func<int, double> func1, Func<int, double> func2) {
+            var n = 1;
+            while (func1.Invoke(n) >= func2.Invoke(n)) {
+                n++;
+            }
+
+            return n;
+        }
+
+        [Test]
+        public void Func1FasterThanFunc2When44() {
+            Func<int, double> func1 = n => 100 * n * n;
+            Func<int, double> func2 = n => Math.Pow(2, n);
+            var min = GetMinNumberThatFunc1FasterThanFunc2(func1, func2);
+
+            Assert.AreEqual(15, min);
+        }
+    }
+}
---
+++ ia/CompareInsertionAndMergeSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IA {
+    public class CompareInsertionAndMergeSort {
+        private int GetTheNumberOfMergeIsFasterThanInsertionSort() {
+            // insertion cost 8 * n ^ 2
+            // merge cost 64 * n * log2(n)
+            var n = 2;
+            while (true) {
+                var insertionCost = 8 * n * n;
+                var mergeCost = 64 * n * Math.Log(n, 2);
+                if (insertionCost > mergeCost)
+                    break;
+                n++;
+            }
+
+            return n;
+        }
+
+        [Test]
+        public void MergeIsFasterThanInsertionWhenSorting44Numbers() {
+            var n = GetTheNumberOfMergeIsFasterThanInsertionSort();
+            Assert.AreEqual(44, n);
+        }
+    }
+}
