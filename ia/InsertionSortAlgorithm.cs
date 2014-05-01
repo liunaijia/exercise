61b6fa52dfa7c9cae65a72895181901a0d97a0ca
liunaijia <lnj@devt.me>
2014-05-01 14:29:21 +0800

    sort

---
+++ ia/InsertionSortAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IA {
+    internal class InsertionSortAlgorithm {
+        public void Sort<T>(IList<T> array) {
+            Sort(array, (x, y) => Comparer.Default.Compare(x, y));
+        }
+
+        public void SortDesc<T>(IList<T> array) {
+            Sort(array, (x, y) => Comparer.Default.Compare(y, x));
+        }
+
+        private void Sort<T>(IList<T> array, Func<T, T, int> comparer) {
+            for (var i = 1; i < array.Count; i++) {
+                var key = array[i];
+                var j = i - 1;
+                while (j >= 0 && comparer.Invoke(array[j], key) > 0) {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
+
+        [Test]
+        public void TestSortNumbers() {
+            var numbers = new[] {3, 4, 1, 10};
+            Sort(numbers);
+
+            Assert.AreEqual(new[] {1, 3, 4, 10}, numbers);
+        }
+
+        [Test]
+        public void TestSortDescNumbers() {
+            var numbers = new[] {3, 4, 1, 10};
+            SortDesc(numbers);
+
+            Assert.AreEqual(new[] {10, 4, 3, 1}, numbers);
+        }
+    }
+}
---
+++ ia/SequenceFindAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IA {
+    class SequenceFindAlgorithm {
+        public int IndexOf<T>(IList<T> array, T value) {
+            for (var i = 0; i < array.Count; i++) {
+                if (Comparer.Default.Compare(array[i], value) == 0)
+                    return i;
+            }
+            return -1;
+        }
+
+        [Test]
+        public void FindExistNumberShouldReturnIndex() {
+            var numbers = new[] {31, 59, 34};
+            var idx = IndexOf(numbers, 59);
+            Assert.AreEqual(1, idx);
+        }
+
+        [Test]
+        public void FindNotExistNumberShouldReturnNegative() {
+            var numbers = new[] { 31, 59, 34 };
+            var idx = IndexOf(numbers, 32);
+            Assert.AreEqual(-1, idx);
+        }
+    }
+}
