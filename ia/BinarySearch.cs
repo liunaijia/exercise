89cdaf8e31c2dcec8e82f3b93b7d2816bcf00c06
liunaijia <lnj@devt.me>
2014-05-03 16:42:55 +0800

    二分法查找

---
+++ ia/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IA {
+    internal class BinarySearch {
+        private int IndexOf<T>(IList<T> collection, T target) {
+            return IndexOf(collection, target, 0, collection.Count);
+        }
+
+        private int IndexOf<T>(IList<T> collection, T target, int start, int end) {
+            if (start > end)
+                return -1;
+            var middle = (start + end) / 2;
+            
+            if (Comparer.Default.Compare(target, collection[middle]) < 0)
+                return IndexOf<T>(collection, target, start, middle - 1);
+
+            if (Comparer.Default.Compare(target, collection[middle]) > 0)
+                return IndexOf<T>(collection, target, middle + 1, end);
+
+            return middle;
+        }
+
+        [Test]
+        public void TestExistsNumber() {
+            const int TEST_TIMES = 10;
+            foreach (var n in Enumerable.Range(0, TEST_TIMES)) {
+                var random = new Random();
+                var numbers = new int[random.Next(100)];
+                for (var i = 0; i < numbers.Length; i++)
+                    numbers[i] = random.Next(100);
+                numbers = numbers.Distinct().OrderBy(_ => _).ToArray();
+
+                for (var i = 0; i < numbers.Length; i++) {
+                    var index = IndexOf(numbers, numbers[i]);
+
+                    Assert.AreEqual(i, index);
+                }
+            }
+        }
+
+        [Test]
+        public void TestNotExistsNumber() {
+            var numbers = new[] {1, 3};
+
+            var index = IndexOf(numbers, 2);
+
+            Assert.AreEqual(-1, index);
+        }
+    }
+}
---
+++ ia/InsertionSortUsingRecursion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IA {
+    class InsertionSortUsingRecursion {
+        private void Sort<T>(IList<T> numbers, int insertNumberIndex = 1) {
+            if (insertNumberIndex < numbers.Count - 1)
+                Sort<T>(numbers, insertNumberIndex + 1);
+            // insert numbers[insertNumberIndex] into sorted numbers[0..insertNumberIndex - 1]
+            var key = numbers[insertNumberIndex];
+            var i = insertNumberIndex - 1;
+            while (i >= 0 && Comparer.Default.Compare(numbers[i], key) > 0) {
+                numbers[i + 1] = numbers[i];
+                i--;
+            }
+            numbers[i + 1] = key;
+        }
+
+        [Test]
+        public void TestSortNumbers() {
+            var numbers = new[] { 3, 4, 1, 10 };
+            Sort(numbers);
+
+            Assert.AreEqual(new[] { 1, 3, 4, 10 }, numbers);
+        }
+
+    }
+}
